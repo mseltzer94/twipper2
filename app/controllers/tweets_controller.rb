71c59429a617bdca60a6ce2b03ed3c4123d80e2c
Michael M. Seltzer <michael@theseltzers.com>
2014-11-22 17:00:18 -0500

    first

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,32 @@
+class TweetsController < ApplicationController
+
+	before_action :authenticate_user!
+
+	def new
+		@tweet = Tweet.new
+		@tweets = current_user.tweets
+	end
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
+		if @tweet.save
+			flash[:success] = "You have a created a tweet"
+		else
+			flash[:danger] = "Please fix the problems below"
+		end
+		#@tweet = Tweet.create(tweet_params)
+		@tweets = current_user.tweets
+		render 'new'
+	end
+
+	private
+
+	def index
+		@tweets =Tweet.all.reject {|tweet| tweet.user == current_user}
+	end
+
+	def tweet_params
+		params.require(:tweet).permit(:tweet)
+	end
+end
